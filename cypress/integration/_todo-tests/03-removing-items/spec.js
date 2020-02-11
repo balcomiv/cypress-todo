89e3a02fdad8aaf350089fe80d8aff50c4d0b92f
Lyle Balcom <balcomiv@gmail.com>
2020-02-11 05:39:51 -0500

    Adding test for 'delete item'.
    1. Added Delete Item Test
    2. Added custom cy command 'addItem'

---
+++ cypress/integration/_todo-tests/03-removing-items/spec.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+describe("Removing items from the todo list", () => {
+  beforeEach(() => {
+    cy.visit("https://sitepoint-editors.github.io/todo-app/");
+  });
+
+  it("can delete an item", () => {
+    // add a few items using custom command
+    cy.addItem("simple");
+    cy.addItem("hard");
+
+    // deletes first item
+    cy.contains("li", "simple")
+      .should("exist")
+      .find(".destroy")
+      // use force: true because we don't want to hover
+      .click({ force: true });
+
+    // confirm deleted item is gone from DOM
+    cy.contains("li", "simple").should("not.exist");
+
+    // confirm other item still exists
+    cy.contains("li", "hard").should("exist");
+  });
+});
--- cypress/integration/_todo-tests/05-xhr/spec.js
+++ cypress/integration/_todo-tests/05-xhr/spec.js
@@ -1,7 +1,23 @@
 /// <reference types="cypress" />
 
-describe("", () => {
-  it("", () => {
+describe("XHR Tests", () => {
+  beforeEach(() => {
     cy.visit("https://sitepoint-editors.github.io/todo-app/");
+  });
+
+  it("starts with zero items (fixture)", () => {
+    // Start Cypress network server
+    // stub route 'GET /todos', return data from fixture file
+    // THEN  visit the page
+    cy.server();
+    cy.route("GET", "/todos/", "fixture:empty-list").as("todos");
+    cy.visit("/");
+    cy.wait("@todos") // wait for 'GET /todos' response
+      // inspect the server's response
+      .its("response.body")
+      .should("have.length", 0);
+
+    // then check the DOM
+    cy.get("li").should("have-length", 0);
   });
 });
