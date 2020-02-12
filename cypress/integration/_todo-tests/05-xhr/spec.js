d0fcfa40a8c281c5da6c13cc10e0dc9f8f00a4c6
Lyle Balcom <balcomiv@gmail.com>
2020-02-12 06:00:01 -0500

    Added XHR tests - Stubs && Spies

--- cypress/integration/_todo-tests/05-xhr/spec.js
+++ cypress/integration/_todo-tests/05-xhr/spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe("XHR Tests", () => {
+describe("XHR Tests - spies && stubs", () => {
   beforeEach(() => {
     cy.visit("https://sitepoint-editors.github.io/todo-app/");
   });
@@ -19,5 +19,39 @@ describe("XHR Tests", () => {
 
     // then check the DOM
     cy.get("li").should("have-length", 0);
+  });
+
+  it("posts new item to the server", () => {
+    cy.server();
+    cy.route("POST", "/todos").as("new-item");
+    cy.visit("/");
+    cy.get(".new-todo").type("test api{enter}");
+    cy.wait("@new-item")
+      .its("request.body") // Use "response.body to check returned item"
+      .should("have.contain", {
+        title: "test api",
+        completed: false
+      });
+  });
+
+  it("shows loading element", () => {
+    // delay XHR to "/todos" by a few seconds
+    // and respod with an empty list
+    cy.server();
+    cy.route({
+      url: "/todos",
+      delay: 2000,
+      response: []
+    }).as("loading");
+    cy.visit("/");
+
+    // shows Loading element
+    cy.get(".loading").should("be.visible");
+
+    // wait for the network call to complete
+    cy.wait("@loading");
+
+    // now the Loading element should go away
+    cy.get(".loading").should("not.be.visible");
   });
 });
